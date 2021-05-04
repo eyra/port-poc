c7d1f11764fe51ba210b2170cc3a2a8b46519b73
Roos Voorvaart <r.voorvaart@uu.nl>
2021-05-04 16:00:33 +0200

    add unit test

--- data_extractor/google_search_history/__init__.py
+++ data_extractor/google_search_history/__init__.py
@@ -13,22 +13,17 @@ def process(data):
     """Return relevant data from browser history pre and post specific date
     Args:
         data: BrowserHistory.json file
-
     Returns:
         dict: dict with summary and DataFrame with extracted data
     """
-
     # Enter date of event X (in this case 'avondklok')
     date = {'ingang_avondklok': np.datetime64(DT.datetime(2021, 1, 23, 21)).view('<i8'),
             'einde_avondklok': np.datetime64(DT.datetime(2021, 4, 28, 4, 30)).view('<i8')}
-
     # Enter news sites
     newssites = 'news.google.com|nieuws.nl|nos.nl|www.rtlnieuws.nl|nu.nl|at5.nl|ad.nl|bd.nl|telegraaf.nl|volkskrant.nl' \
         '|parool.nl|metronieuws.nl|nd.nl|nrc.nl|rd.nl|trouw.nl'
-
     # Extract moment (pre/during/after event X) and website (news/other)
     results = {'Moment': [], 'Website': []}
-
     for data_unit in data["Browser History"]:
         if data_unit["time_usec"] < date['ingang_avondklok']:
             results['Moment'].append('Voor avondklok')
@@ -36,19 +31,15 @@ def process(data):
             results['Moment'].append('Na avondklok')
         else:
             results['Moment'].append('Tijdens avondklok')
-
         if re.findall(newssites, data_unit["url"]):
             results['Website'].append('Nieuws')
         else:
             results['Website'].append('Anders')
-
     # Put results in DataFrame
     results = pd.DataFrame(results)
-
     # Count number of news vs. other websites per moment
     data_frame = results.groupby(
         ['Moment', 'Website']).size().reset_index(name='Aantal')
-
     return {
         "summary": f"The following files where read: BrowserHistory.json.",
         "data": data_frame
--- data_extractor/tests/test_google_search_history.py
+++ data_extractor/tests/test_google_search_history.py
@@ -1,9 +1,55 @@
+"""Test data extraction from Google Browser History .json file"""
+
+import pandas as pd
 from google_search_history import process
-from pathlib import Path
 
-DATA_PATH = Path(__file__).parent / "data"
+DATA = {
+    "Browser History": [
+        {
+            "page_transition": "LINK",
+            "title": "title1",
+            "url": "https://www.nu.nl/coronavirus/6131452/rivm-afgelopen-week-minder-getest-percentage-positief-stijgt.html",
+            "client_id": "client_id1",
+            "time_usec": 1611262800000000},
+        {
+            "page_transition": "LINK",
+            "title": "title2",
+            "url": "https://www.uu.nl/",
+            "client_id": "client_id2",
+            "time_usec": 1611349200000000},
+        {
+            "page_transition": "LINK",
+            "title": "title3",
+            "url": "https://nos.nl/artikel/2379383-rivm-bezorgd-om-hoge-coronacijfers-onder-40-tot-60-jarigen",
+            "client_id": "client_id3",
+            "time_usec": 1614373200000000},
+        {
+            "page_transition": "LINK",
+            "title": "title4",
+            "url": "https://www.ns.nl/",
+            "client_id": "client_id4",
+            "time_usec": 1614286800000000},
+        {
+            "page_transition": "LINK",
+            "title": "title5",
+            "url": "https://www.nrc.nl/nieuws/2021/05/03/coronablog-4-mei-a4042259",
+            "client_id": "client_id5",
+            "time_usec": 1619816400000000},
+        {
+            "page_transition": "LINK",
+            "title": "title6",
+            "url": "https://www.bol.com/nl/p/jan-van-haasteren-ontbrekende-stukje-puzzel-1000-stukjes/9300000031132593/?bltgh=mjRTtZln6O-wVT40MgZKoA.4_12.13.ProductImage",
+            "client_id": "client_id6",
+            "time_usec": 1619730000000000}
+    ]
+}
 
 
-def test_gslh():
-    result = process(DATA_PATH.joinpath("BrowserHistory.json").open("rb"))
-    assert result["summary"] == 'The following files where read: BrowserHistory.json.'
+def test_process():
+    result = process(DATA)
+    print(result["data"])
+
+    expected = {'Moment': {0: 'Na avondklok', 1: 'Na avondklok', 2: 'Tijdens avondklok', 3: 'Tijdens avondklok', 4: 'Voor avondklok', 5: 'Voor avondklok'}, 'Website': {
+        0: 'Anders', 1: 'Nieuws', 2: 'Anders', 3: 'Nieuws', 4: 'Anders', 5: 'Nieuws'}, 'Aantal': {0: 1, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1}}
+
+    assert result["data"].to_dict() == expected
