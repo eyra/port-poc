4ea527bfd6b004d431443ab8963e5fda9186bd14
Roos Voorvaart <r.voorvaart@uu.nl>
2021-05-04 15:04:54 +0200

    Restructure process/main

--- data_extractor/google_search_history/__init__.py
+++ data_extractor/google_search_history/__init__.py
@@ -1,3 +1,5 @@
+""" Script to extract info from Google Browser History """
+
 __version__ = '0.1.0'
 
 import json
@@ -7,25 +9,24 @@ import pandas as pd
 import re
 
 
-def process(file_data):
+def process(data):
     """Return relevant data from browser history pre and post specific date
     Args:
-        file_data: BrowserHistory.json file
-        date: dictionary with date cut-offs
+        data: BrowserHistory.json file
 
     Returns:
-        pd.DataFrame: dataframe with relevant info
+        dict: dict with summary and DataFrame with extracted data
     """
 
-    with open(file_data, encoding='utf-8-sig') as f:
-        data = json.load(f)
-
+    # Enter date of event X (in this case 'avondklok')
     date = {'ingang_avondklok': np.datetime64(DT.datetime(2021, 1, 23, 21)).view('<i8'),
             'einde_avondklok': np.datetime64(DT.datetime(2021, 4, 28, 4, 30)).view('<i8')}
 
+    # Enter news sites
     newssites = 'news.google.com|nieuws.nl|nos.nl|www.rtlnieuws.nl|nu.nl|at5.nl|ad.nl|bd.nl|telegraaf.nl|volkskrant.nl' \
         '|parool.nl|metronieuws.nl|nd.nl|nrc.nl|rd.nl|trouw.nl'
 
+    # Extract moment (pre/during/after event X) and website (news/other)
     results = {'Moment': [], 'Website': []}
 
     for data_unit in data["Browser History"]:
@@ -41,15 +42,23 @@ def process(file_data):
         else:
             results['Website'].append('Anders')
 
+    # Put results in DataFrame
     results = pd.DataFrame(results)
 
+    # Count number of news vs. other websites per moment
+    data_frame = results.groupby(
+        ['Moment', 'Website']).size().reset_index(name='Aantal')
+
     return {
-        "summary": f"The following files where read: {file_data}.",
-        "data": results.groupby(['Moment', 'Website']).size().reset_index(name='Aantal')
+        "summary": f"The following files where read: BrowserHistory.json.",
+        "data": data_frame
     }
 
 
 if __name__ == '__main__':
 
-    result = process("tests/data/BrowserHistory.json")
+    with open(file_data, encoding='utf-8-sig') as f:
+        data = json.load(f)
+
+    result = process(data)
     print(result)
