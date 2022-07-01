435e111a49202ef3893a8e6ff971fbc3f9924b87
Shiva Nadi <s.nadi@uu.nl>
2022-07-02 00:45:28 +0200

    adapt to new output format

--- data_extractor/tests/test_whatsapp_chat.py
+++ data_extractor/tests/test_whatsapp_chat.py
@@ -1,29 +1,34 @@
-from whatsapp_chat import process
-from whatsapp_chat import anonymize_participants
+from data_extractor.whatsapp_chat import process
+from data_extractor.whatsapp_chat import anonymize_participants
+from data_extractor.whatsapp_chat import get_df_per_participant
 from pathlib import Path
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
 
+
+
 DATA_PATH = Path(__file__).parent / "data"
 
 EXPECTED = [
-    {'username': 'user1', 'total_words_no': 20, 'url_no': 1, 'location_no': 1, 'file_no': 0, 'message_no': 3,
-     'first_message_date': '2022-03-16 15:20:25', 'last_message_date': '2022-03-24 20:19:38', 'out_degree': 2,
-     'in_degree': 3, 'user_reply2': 'user2', 'reply_2_user': 'user2'},
+    {'username': 'person1', 'Total number of words': 20, 'Number of URLs': 1, 'Number of shared locations': 1, 'file_no': 0, 'Number of messages': 3,
+     'Date first message': pd.to_datetime('2022-03-16 15:20:25'), 'Date last message': pd.to_datetime('2022-03-24 20:19:38')},
 
-    {'username': 'user2', 'total_words_no': 18, 'url_no': 2, 'location_no': 0, 'file_no': 0, 'message_no': 3,
-     'first_message_date': '2022-03-16 15:25:38', 'last_message_date': '2022-03-26 18:52:15', 'out_degree': 3,
-     'in_degree': 3, 'user_reply2': 'user1', 'reply_2_user': 'user1'},
+    {'username': 'person2', 'Total number of words': 7, 'Number of URLs': 1, 'Number of shared locations': 0, 'file_no': 0, 'Number of messages': 3,
+     'Date first message': pd.to_datetime('2022-03-16 15:25:38'), 'Date last message': pd.to_datetime('2022-03-26 18:52:15')},
 
-    {'username': 'user3', 'total_words_no': 1, 'url_no': 0, 'location_no': 0, 'file_no': 0, 'message_no': 1,
-     'first_message_date': '2022-03-16 15:26:48', 'last_message_date': '2022-03-16 15:26:48', 'out_degree': 1,
-     'in_degree': 1, 'user_reply2': 'user2', 'reply_2_user': 'user2'},
+    {'username': 'person3', 'Total number of words': 1, 'Number of URLs': 0, 'Number of shared locations': 0, 'file_no': 0, 'Number of messages': 1,
+     'Date first message': pd.to_datetime('2022-03-16 15:26:48'), 'Date last message': pd.to_datetime('2022-03-16 15:26:48')},
 
-    {'username': 'user4', 'total_words_no': 21, 'url_no': 0, 'location_no': 0, 'file_no': 0, 'message_no': 2,
-     'first_message_date': '2020-07-14 22:05:54', 'last_message_date': '2022-03-20 20:08:51', 'out_degree': 2,
-     'in_degree': 1, 'user_reply2': 'user1', 'reply_2_user': 'user1'}
+    {'username': 'person4', 'Total number of words': 21, 'Number of URLs': 0, 'Number of shared locations': 0, 'file_no': 0, 'Number of messages': 2,
+     'Date first message': pd.to_datetime('2020-07-14 22:05:54'), 'Date last message': pd.to_datetime('2022-03-20 20:08:51')}
 ]
+
+# EXPECTED_1 = {'Description': ['Total number of words', 'Number of messages', 'Date first message', 'Date last message',
+#                               'Number of URLs', 'Number of shared locations'],
+#               'Value': [20, 3, pd.to_datetime('2022-03-16 15:20:25'), pd.to_datetime('2022-03-24 20:19:38'), 1, 1]}
+#
+# df_expected_1 = pd.DataFrame(data=EXPECTED_1)
 
 
 def test_process():
@@ -36,15 +41,54 @@ def test_process():
 
     df_expected = pd.DataFrame(EXPECTED)
     df_expected = anonymize_participants(df_expected)
-    df_expected['message_no'] = df_expected['message_no'].astype('int64')
-    df_expected['url_no'] = df_expected['url_no'].astype('int32')
-    df_expected['location_no'] = df_expected['location_no'].astype('int32')
+    df_expected['Number of messages'] = df_expected['Number of messages'].astype('int64')
+    df_expected['Number of URLs'] = df_expected['Number of URLs'].astype('int32')
+    df_expected['Number of shared locations'] = df_expected['Number of shared locations'].astype('int32')
     df_expected['file_no'] = df_expected['file_no'].astype('int32')
 
+    results = []
+    df_melt = pd.melt(df_expected, id_vars=["username"],
+                      value_vars=["Total number of words", "Number of messages", "Date first message", "Date last message",
+                                  "Number of URLs", "file_no", "Number of shared locations"], var_name='Description', value_name='Value')
+
+    usernames = df_melt["username"].unique()
+    for u in usernames:
+        df_user = df_melt[(df_melt["username"] == u) & df_melt["Value"] != 0]
+        results.append(df_user)
+
+    expected_results = []
+    for df in results:
+        user_name = pd.unique(df["username"])[0]
+        expected_results.append(
+            {
+                "id": user_name,  # "overview",
+                "title": user_name,  # "The following data is extracted from the file:",
+                "data_frame": df[["Description", "Value"]].reset_index(drop=True)
+            }
+        )
+    # print(type(expected_results[0]["data_frame"]))
+    # print(expected_results[1]["data_frame"])
+    # print('******')
+
     # result = process(DATA_PATH.joinpath("whatsapp_chat.zip"))
-    result = process(DATA_PATH.joinpath("_chat.txt"))
-    assert len(result) == 1
-    df_result = result[0]["data_frame"]
-    assert_frame_equal(df_result, df_expected)
+    df_result = process(DATA_PATH.joinpath("_chat.txt"))
+    # print(df_result[1]["data_frame"])
+    assert_frame_equal(df_result[0]["data_frame"], expected_results[0]["data_frame"])
+    assert_frame_equal(df_result[1]["data_frame"], expected_results[1]["data_frame"])
+    assert_frame_equal(df_result[2]["data_frame"], expected_results[2]["data_frame"])
+    assert_frame_equal(df_result[3]["data_frame"], expected_results[3]["data_frame"])
+
+    # print(type(df_result[0]["data_frame"]))
+    # print(df_result[0]["data_frame"])
+    # print(df_expected_1)
+    #
+    # # assert len(result_file) == 1
+    #
+    #
+    # assert_frame_equal(df_result[0]["data_frame"], df_expected_1)
+
+
+if __name__ == "__main__":
+    test_process()
 
 
