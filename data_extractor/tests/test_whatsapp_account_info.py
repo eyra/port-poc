f01eba6bf562233b8f02c39dd81eabd02c35ff2a
Shiva Nadi <s.nadi@uu.nl>
2022-08-19 11:55:10 +0200

    Fix pylint issues

--- data_extractor/tests/test_whatsapp_account_info.py
+++ data_extractor/tests/test_whatsapp_account_info.py
@@ -1,8 +1,10 @@
+""" Test script for the whatsapp_account_info script"""
+
+from pathlib import Path
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
 from whatsapp_account_info import process
-from pathlib import Path
 
 
 DATA_PATH = Path(__file__).parent / "data"
@@ -15,7 +17,8 @@ EXPECTED = [
 
 def test_process():
     """ Test process function.
-        compares the expected dataframe with the output of the process function to check if all the columns are matched.
+        compares the expected dataframe with the output of the process function
+        to check if all the columns are matched.
         Raises
         -------
         AssertionError: When provided expected dataframe could not match the participants dataframe
@@ -25,4 +28,3 @@ def test_process():
     result = process(DATA_PATH.joinpath("account_info.zip"))
     df_result = result[0]["data_frame"]
     assert_frame_equal(df_result, df_expected)
-
