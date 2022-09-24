a21e929358b1b1eabef258d84d517fa1cb074aef
emielvdveen <emiel@applike.nl>
2022-09-24 11:55:08 +0200

    Intoduction of dynamic scripts: user prompts controlled by python scripts
    Note: Removed local testing of script inside browser. This v3 version is just to support last version of whatsapp script before moving over to the new production repo: https://github.com/eyra/port

--- data_extractor/example/__init__.py
+++ data_extractor/example/__init__.py
@@ -1,20 +1,76 @@
-__version__ = '0.2.0'
-
-import zipfile
 import pandas as pd
 
 
-def process(file_data):
-    names = []
-    zfile = zipfile.ZipFile(file_data)
-    data = []
-    for name in zfile.namelist():
-        names.append(name)
-        info = zfile.getinfo(name)
-        data.append((name, info.compress_size, info.file_size))
+def process():
+    chat_file_name = yield prompt_file()
+    usernames = extract_usernames(chat_file_name)
+    username = yield prompt_radio(usernames)
+    yield result(usernames, username)
 
-    return [{
+
+def prompt_file():
+    return {
+        "cmd": "prompt",
+        "prompt": {
+            "type": "file",
+            "file": {
+                "title": {
+                    "en": "Step 1: Select the chat file",
+                    "nl": "Stap 1: Selecteer het chat file"
+                },
+                "description": {
+                    "en": "We previously asked you to export a chat file from Whatsapp. Please select this file so we can extract relevant information for our research.",
+                    "nl": "We hebben je gevraagd een chat bestand te exporteren uit Whatsapp. Je kan deze file nu selecteren zodat wij er relevante informatie uit kunnen halen voor ons onderzoek."
+                },
+                "extensions": "application/zip, text/plain",
+            }
+        }
+    }
+
+
+def prompt_radio(usernames):
+    return {
+        "cmd": "prompt",
+        "prompt": {
+            "type": "radio",
+            "radio": {
+                "title": {
+                    "en": "Step 2: Select your username",
+                    "nl": "Stap 2: Selecteer je gebruikersnaam"
+                },
+                "description": {
+                    "en": "The following users are extracted from the chat file. Which one are you?",
+                    "nl": "De volgende gebruikers hebben we uit de chat file gehaald. Welke ben jij?"
+                },
+                "items": usernames,
+            }
+        }
+    }
+
+
+def extract_usernames(chat_file_name):
+    print(f"filename: {chat_file_name}")
+
+    with open(chat_file_name) as chat_file:
+        while (line := chat_file.readline().rstrip()):
+            print(line)
+
+    return ["emielvdveen", "a.m.mendrik", "9bitcat"]
+
+
+def result(usernames, selected_username):
+    data = []
+    for username in usernames:
+        description = "you" if username == selected_username else "-"
+        data.append((username, description))
+
+    data_frame = pd.DataFrame(data, columns=["username", "description"])
+
+    print(data_frame)
+
+    result = [{
         "id": "overview",
-        "title": "The following files where read:",
-        "data_frame": pd.DataFrame(data, columns=["filename", "compressed size", "size"])
+        "title": "The following usernames where extracted:",
+        "data_frame": data_frame
     }]
+    return {"cmd": "result", "result": result}
