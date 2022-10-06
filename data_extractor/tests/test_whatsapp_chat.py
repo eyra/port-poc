7f612b8a6e9b8435c88496cfb1881f997e3851da
Shiva Nadi <s.nadi@uu.nl>
2022-10-06 23:23:07 +0200

    Add username

--- data_extractor/tests/test_whatsapp_chat.py
+++ data_extractor/tests/test_whatsapp_chat.py
@@ -7,8 +7,12 @@ import pandas as pd
 
 from whatsapp_chat import process
 from whatsapp_chat import anonymize_participants
+
 from pandas.testing import assert_frame_equal
 
+
+DATA_PATH = Path(__file__).parent / "data"
+FILES_TO_TEST = [ p.name for p in DATA_PATH.glob("*_chat*.txt")]
 
 class Dutch_Const:
     """Access class constants using variable ``DUTCH_CONST``."""
@@ -19,46 +23,43 @@ class Dutch_Const:
 
 DUTCH_CONST = Dutch_Const()
 
-DATA_PATH = Path(__file__).parent / "data"
-FILES_TO_TEST = [p.name for p in DATA_PATH.glob("*_chat*.txt")]
 
 EXPECTED = [
-    {'username': 'person1', 'Total number of words': 20, 'Number of URLs': 1,
+    {'username': 'person1', 'Total number of words': 21, 'Number of URLs': 1,
      'Number of shared locations': 1, 'file_no': 0, 'Number of messages': 3,
-     'Date first message': pd.to_datetime('2022-03-16 15:20:00'),
-     'Date last message': pd.to_datetime('2022-03-24 20:19:00'),
+     'Date first message': pd.to_datetime('2022-03-16 15:20:25'),
+     'Date last message': pd.to_datetime('2022-03-24 20:19:38'),
      'Who do you most often reply to?': 'person2',
      'Who replies to you the most often?': 'person2'},
 
     {'username': 'person2', 'Total number of words': 7, 'Number of URLs': 1,
      'Number of shared locations': 0, 'file_no': 0, 'Number of messages': 3,
-     'Date first message': pd.to_datetime('2022-03-16 15:25:00'),
-     'Date last message': pd.to_datetime('2022-03-26 18:52:00'),
+     'Date first message': pd.to_datetime('2022-03-16 15:25:38'),
+     'Date last message': pd.to_datetime('2022-03-26 18:52:15'),
      'Who do you most often reply to?': 'person1',
      'Who replies to you the most often?': 'person1'},
 
     {'username': 'person3', 'Total number of words': 1, 'Number of URLs': 0,
      'Number of shared locations': 0, 'file_no': 0, 'Number of messages': 1,
-     'Date first message': pd.to_datetime('2022-03-16 15:26:00'),
-     'Date last message': pd.to_datetime('2022-03-16 15:26:00'),
+     'Date first message': pd.to_datetime('2022-03-16 15:26:48'),
+     'Date last message': pd.to_datetime('2022-03-16 15:26:48'),
      'Who do you most often reply to?': 'person2',
      'Who replies to you the most often?': 'person2'},
 
-    {'username': 'person4', 'Total number of words': 21, 'Number of URLs': 0,
+    {'username': 'person4', 'Total number of words': 22, 'Number of URLs': 0,
      'Number of shared locations': 0, 'file_no': 0, 'Number of messages': 2,
-     'Date first message': pd.to_datetime('2020-07-14 22:05:00'),
-     'Date last message': pd.to_datetime('2022-03-20 20:08:00'),
+     'Date first message': pd.to_datetime('2020-07-14 22:05:54'),
+     'Date last message': pd.to_datetime('2022-03-20 20:08:51'),
      'Who do you most often reply to?': 'person1',
      'Who replies to you the most often?': 'person1'}
 ]
 
 
-def process_data(filename: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    """ 
-    Returns a tuple contaning the excepted output dataframe, 
+def process_data(filename: str, person_index: int) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Returns a tuple contaning the excepted output dataframe,
     and the dataframe from the process function
     """
-
     donor_user_name = "person2"
 
     df_expected = pd.DataFrame(EXPECTED)
@@ -83,8 +84,6 @@ def process_data(filename: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
                       var_name='Description', value_name='Value')
 
     usernames = sorted(set(df_melt["username"]))
-
-    # bring donator username to the top of the list
     usernames.insert(0, usernames.pop(usernames.index(DUTCH_CONST.YOU)))
 
     for user in usernames:
@@ -102,39 +101,46 @@ def process_data(filename: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
             }
         )
 
-    # file_to_test = DATA_PATH.joinpath(filename)
-    # df_result = process(str(file_to_test))
+    file_to_test = DATA_PATH.joinpath(filename)
     flow = process()
     # start flow and handle first prompt
     file_prompt = flow.send(None)
     assert file_prompt["cmd"] == 'prompt'
     assert file_prompt["prompt"]["type"] == 'file'
 
-    selected_filename = DATA_PATH.joinpath(filename)
-    radio_prompt = flow.send(str(selected_filename))
+    radio_prompt = flow.send(str(file_to_test))
     assert radio_prompt["cmd"] == 'prompt'
     assert radio_prompt["prompt"]["type"] == 'radio'
 
     result = flow.send(donor_user_name)
-    df_result = result["result"][0]["data_frame"]
 
-    df_expected = expected_results[0]["data_frame"]
+    # print(result["result"][person_index]["data_frame"])
+    # print('#########')
+    # print(expected_results[person_index]["data_frame"])
 
-    return df_result, df_expected
+    return result["result"][person_index], expected_results[person_index]
 
 
 # Generate test conditions
-conditions = list(itertools.product(FILES_TO_TEST, range(1)))
+conditions = list(itertools.product(FILES_TO_TEST, range(4), range(7)))
 
 
-@pytest.mark.parametrize("filename, condition_index", conditions)
-def test_process(filename: str, condition_index: int):
-    """ 
+@pytest.mark.parametrize("filename,person_index,condition_index", conditions)
+def test_process(filename: str, person_index: int, condition_index: int):
+    """
     Compares the expected dataframe with the output of the process function
     """
 
-    df_result, df_expected_results = process_data(filename)
-    assert_frame_equal(df_result, df_expected_results)
+    df_result, expected_results = process_data(filename, person_index)
+    df_expected_results = expected_results["data_frame"]
+    df_result = expected_results["data_frame"]
 
 
+    # check whether the condition can be tested
+    try:
+        description, expected_value = tuple(df_expected_results[["Description", "Value"]].iloc[condition_index])
+        description_result, value = tuple(df_result[["Description", "Value"]].iloc[condition_index])
+    except:
 
+        return
+    assert value == expected_value, f"In {filename} for person {person_index}, test: {description} FAILED, {value} != {expected_value}"
