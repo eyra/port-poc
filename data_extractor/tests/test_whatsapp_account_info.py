0f48ae4613430871b14ceafc613b689ff4979198
Shiva Nadi <s.nadi@uu.nl>
2022-11-04 10:41:02 +0100

    Add file_prompt functionality

--- data_extractor/tests/test_whatsapp_account_info.py
+++ data_extractor/tests/test_whatsapp_account_info.py
@@ -8,9 +8,10 @@ from whatsapp_account_info import process
 
 
 DATA_PATH = Path(__file__).parent / "data"
+
 EXPECTED = [
-    {'number_of_groups': 4,
-     'number_of_contacts': 3
+    {'Aantal groepen': 4,
+     'Aantal contacten': 3
      }
 ]
 
@@ -24,7 +25,17 @@ def test_process():
         AssertionError: When provided expected dataframe could not match the participants dataframe
         """
     df_expected = pd.DataFrame(EXPECTED)
+    file_to_test = DATA_PATH.joinpath("account_info.zip")
 
-    result = process(DATA_PATH.joinpath("account_info.zip"))
-    df_result = result[0]["data_frame"]
-    assert_frame_equal(df_result, df_expected)
+    flow = process()
+
+    file_prompt = flow.send(None)
+    assert file_prompt["cmd"] == 'prompt'
+    assert file_prompt["prompt"]["type"] == 'file'
+
+    file_prompt = flow.send(str(file_to_test))
+    assert file_prompt[0]["id"] == 'Whatsapp account info'
+    assert file_prompt[0]["title"] == 'Het account informatie bestand bestaat uit:'
+    assert_frame_equal(file_prompt[0]["data_frame"], df_expected)
+
+
