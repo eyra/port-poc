2d5154b3016cf3d832dee4443270b955bbfba6cc
Shiva Nadi <s.nadi@uu.nl>
2022-11-04 22:54:50 +0100

    Prametrize test function

--- data_extractor/tests/test_whatsapp_account_info.py
+++ data_extractor/tests/test_whatsapp_account_info.py
@@ -4,10 +4,14 @@ from pathlib import Path
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
+import pytest
+
 from whatsapp_account_info import process
 
 
-DATA_PATH = Path(__file__).parent / "data"
+DATA_PATH = Path(__file__).parent / "data/account_info"
+FILES_TO_TEST = [p.name for p in DATA_PATH.glob("*.zip")]
+
 
 EXPECTED = [
     {'Aantal groepen': 4,
@@ -16,7 +20,8 @@ EXPECTED = [
 ]
 
 
-def test_process():
+@pytest.mark.parametrize("filename_to_test", FILES_TO_TEST)
+def test_process(filename_to_test):
     """ Test process function.
         compares the expected dataframe with the output of the process function
         to check if all the columns are matched.
@@ -25,7 +30,7 @@ def test_process():
         AssertionError: When provided expected dataframe could not match the participants dataframe
         """
     df_expected = pd.DataFrame(EXPECTED)
-    file_to_test = DATA_PATH.joinpath("account_info.zip")
+    file_to_test = DATA_PATH.joinpath(filename_to_test)
 
     flow = process()
 
@@ -34,8 +39,4 @@ def test_process():
     assert file_prompt["prompt"]["type"] == 'file'
 
     file_prompt = flow.send(str(file_to_test))
-    assert file_prompt[0]["id"] == 'Whatsapp account info'
-    assert file_prompt[0]["title"] == 'Het account informatie bestand bestaat uit:'
     assert_frame_equal(file_prompt[0]["data_frame"], df_expected)
-
-
